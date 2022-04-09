744a38109b18a8ea9d4ea3675d02d74d02fef355
ZSendokame <70088953+ZSendokame@users.noreply.github.com>
2022-04-08 22:16:59 -0400

    Create api.py

---
+++ api.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, render_template, send_file, request
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def notFound(status):
+    return render_template('404.html', path=request.path)
+
+@app.get('/')
+def root():
+    fileList = os.listdir('./files')
+
+    return render_template(
+        'index.html',
+        fileLenght=len(fileList),
+        file=fileList
+    )
+
+@app.get('/readme.md')
+def readme():
+    return render_template('readme.html')
+
+@app.get('/download/<filename>')
+def returnFile(filename):
+    return send_file('./files//' + filename)
+
+app.run()
